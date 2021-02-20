94b7e6378f8feab8d105c1814275bbec4e7d86b9
Paul Chen <paulchen647@gmail.com>
2021-02-20 14:53:28 -0800

    Format time to HH:MM:SS

--- client/src/components/Counter.js
+++ client/src/components/Counter.js
@@ -12,39 +12,56 @@ class Counter extends React.Component {
     this.handleStartTimer = this.handleStartTimer.bind(this);
     this.handlePauseTimer = this.handlePauseTimer.bind(this);
     this.decrement = this.decrement.bind(this);
+    this.formatTime = this.formatTime.bind(this);
   }
-  handleStartTimer(){
-    console.log('Starting timer.');
-    if(this.state.time === 0){
-      this.setState({time: 5,started: true});
+  handleStartTimer() {
+    console.log("Starting timer.");
+    if (this.state.time === 0) {
+      this.setState({ time: 5, started: true });
       clearInterval(this.timer);
       this.timer = setInterval(this.decrement, 1000);
-    } else if (this.state.paused){
-      this.setState({paused: false})
-    };
-    
+    } else if (this.state.paused) {
+      this.setState({ paused: false });
+    }
+
     // if(this.state.time)
   }
-  handlePauseTimer(){
-    this.setState({paused: true})
+  handlePauseTimer() {
+    this.setState({ paused: true });
   }
-  decrement(){
-    if(this.state.time > 0 && !this.state.paused){
-      this.setState({time: this.state.time - 1});
-    } else if(this.state.time === 0){
-      this.setState({started: false, paused: false})
+  decrement() {
+    if (this.state.time > 0 && !this.state.paused) {
+      this.setState({ time: this.state.time - 1 });
+    } else if (this.state.time === 0) {
+      this.setState({ started: false, paused: false });
     }
   }
+  formatTime(seconds) {
+    const date = new Date(null);
+    date.setSeconds(seconds);
+    return date.toISOString().substr(11, 8);
+  }
 
-  
   render() {
-    return (<div>
-      <h2>Counter</h2>
-      <h3> Time Remaining</h3>
-      <p>{this.state.time}</p>
-      <button onClick={this.handleStartTimer} disabled={this.state.started && !this.state.paused}>{this.state.started ? 'Resume':'Start Timer'}</button>
-      <button onClick={this.handlePauseTimer} disabled={this.state.paused || !this.state.started}>Pause</button>
-    </div>);
+    return (
+      <div>
+        <h2>Counter</h2>
+        <h3> Time Remaining</h3>
+        <p>{this.formatTime(this.state.time)}</p>
+        <button
+          onClick={this.handleStartTimer}
+          disabled={this.state.started && !this.state.paused}
+        >
+          {this.state.started ? "Resume" : "Start Timer"}
+        </button>
+        <button
+          onClick={this.handlePauseTimer}
+          disabled={this.state.paused || !this.state.started}
+        >
+          Pause
+        </button>
+      </div>
+    );
   }
 }
 export default Counter;
