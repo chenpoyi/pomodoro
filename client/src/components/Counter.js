e9c139ca0370c5b0d6fbac2f924b203669970e31
Paul Chen <paulchen647@gmail.com>
2021-02-20 14:34:41 -0800

    Implement pause function

--- client/src/components/Counter.js
+++ client/src/components/Counter.js
@@ -6,36 +6,44 @@ class Counter extends React.Component {
     this.state = {
       time: 0,
       started: false,
-      paused: false
+      paused: false,
+      timer: 0,
     };
     this.handleStartTimer = this.handleStartTimer.bind(this);
+    this.handlePauseTimer = this.handlePauseTimer.bind(this);
     this.decrement = this.decrement.bind(this);
   }
   handleStartTimer(){
     console.log('Starting timer.');
-    this.setState({time: 5,started: true});
-    setInterval(this.decrement, 1000);
+    if(this.state.time === 0){
+      this.setState({time: 5,started: true});
+      clearInterval(this.timer);
+      this.timer = setInterval(this.decrement, 1000);
+    } else if (this.state.paused){
+      this.setState({paused: false})
+    };
+    
     // if(this.state.time)
   }
-
+  handlePauseTimer(){
+    this.setState({paused: true})
+  }
   decrement(){
     if(this.state.time > 0 && !this.state.paused){
       this.setState({time: this.state.time - 1});
-    } else {
+    } else if(this.state.time === 0){
       this.setState({started: false, paused: false})
     }
   }
 
-  pause(){
-    this.setState({paused: true})
-  }
+  
   render() {
     return (<div>
       <h2>Counter</h2>
       <h3> Time Remaining</h3>
       <p>{this.state.time}</p>
-      <button onClick={this.handleStartTimer} disabled={this.state.started}>Start Timer</button>
-      <button onClick={this.paused} disabled={this.state.paused || !this.state.started}>Pause</button>
+      <button onClick={this.handleStartTimer} disabled={this.state.started && !this.state.paused}>{this.state.started ? 'Resume':'Start Timer'}</button>
+      <button onClick={this.handlePauseTimer} disabled={this.state.paused || !this.state.started}>Pause</button>
     </div>);
   }
 }
